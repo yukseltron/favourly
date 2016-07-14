7e35471726c687e36291293fd4c9db0116209cd0
Adithya Sukumar <emailadithya@gmail.com>
2016-07-14 15:15:02 -0700

    Add files via upload

--- Parser.py
+++ Parser.py
@@ -26,7 +26,12 @@ class Parser:
                 if "points" in wl[i] or "pts" in wl[i]:
                     self.points = wl[i-1]
                 if "do" in wl[i]:
-                    self.action = wl[i+1]
+                    sentence = ""
+                    n = 1
+                    while wl[i+n] != "for":
+                        sentence += wl[i+n] + " "
+                        n += 1
+                    self.action = sentence[:(len(sentence)-1)]
             self.tup = [self.action, self.timestamp, self.user, self.points]
         elif wl[0] == "completed":
             self.type = 1
